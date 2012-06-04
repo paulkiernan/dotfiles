61c96978707eada50607269f90ccd68669a83f94
Paul Kiernan <paul.kiernan@moat.com>
2012-06-04 18:27:06 -0400

    Update to the bootstrap script itself...

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,37 +1,28 @@
 #!/bin/bash
 
+# Use this file to (re)build a familiar command line env
+
 # THIS IS A WORK IN PROGRESS
 # BE CAREFUL, DAMMIT
 
 # Don't forget the SSH keys.
-# Setuptools needs to be installed.
+# Need to sudo to sucessfully apt-get
 
-mkdir -p lib/hg
-mkdir -p lib/python
-mkdir -p lib/virtualenvs
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get -y install sl zsh vim chromium-browser ipython
+sudo apt-get -y install python-setuptools
+
 mkdir bin
 mkdir src
 
-hg clone 'http://selenic.com/repo/hg#stable' ~/lib/hg/hg-stable
-cd ~/lib/hg/hg-stable
-make local
-cd
-export PATH="$HOME/lib/hg/hg-stable:$PATH"
-
-hg clone https://bitbucket.org/paul_kiernan/dotfiles
 git clone git://github.com/sjl/oh-my-zsh ~/lib/oh-my-zsh
 git clone git://github.com/sjl/z-zsh ~/lib/z
 
-hg clone http://bitbucket.org/ianb/pip/ ~/lib/python/pip
-cd ~/lib/python/pip
-sudo python setup.py install
-cd
+cd lib/
 
-sudo pip install virtualenv
+git clone https://github.com/paulkiernan/dotfiles
 
-hg clone http://bitbucket.org/dhellmann/virtualenvwrapper ~/lib/python/virtualenvwrapper
-cd ~/lib/python/virtualenvwrapper
-sudo python setup.py install
 cd
 
 ln -s "$HOME/lib/dotfiles/zsh" "$HOME/lib/oh-my-zsh/custom"
@@ -47,16 +38,3 @@ rm ~/.zshrc
 rm ~/.bashrc
 rm ~/.bash_profile
 ln -s "$HOME/lib/dotfiles/.zshrc" "$HOME/.zshrc"
-
-hg clone bb://sjl/hg-prompt/ "$HOME/lib/hg/hg-prompt"
-hg clone bb://sjl/hg-paste/ "$HOME/lib/hg/hg-paste"
-hg clone bb://sjl/hg-review/ "$HOME/src/hg/hg-review"
-hg clone https://bitbucket.org/peerst/hgcollapse "$HOME/lib/hg/hgcollapse"
-hg clone bb://durin42/histedit/ "$HOME/lib/hg/histedit"
-hg clone bb://durin42/hg-git/ "$HOME/lib/hg/hg-git"
-hg clone bb://Bill_Barry/hgattic "$HOME/lib/hg/hgattic"
-hg clone bb://edgimar/crecord "$HOME/lib/hg/crecord"
-hg clone https://bitbucket.org/durin42/hgsubversion "$HOME/lib/hg/hgsubversion"
-
-git clone git://github.com/jelmer/dulwich.git "$HOME/lib/dulwich"
-ln -s "$HOME/lib/dulwich/dulwich" "$HOME/lib/hg/hg-stable/dulwich"
