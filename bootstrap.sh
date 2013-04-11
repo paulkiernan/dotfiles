b7dfd00aefd94cf7875daa2b3f17cabf2fba500e
Paul Kiernan <paul.kiernan@moat.com>
2013-04-11 14:07:47 -0400

    Move all references to lib to dotfiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@
 # Need to sudo to sucessfully apt-get
 
 echo "Creating the local lib directory for dotfiles"
-mkdir ~/lib/dotfiles/
+mkdir ~/.dotfiles/
 
 # Install essential dev tools
 sudo apt-get -y install sl curl bash-completion build-essential zsh vim byobu elinks
@@ -16,28 +16,25 @@ sudo apt-get -y install ipython bpython python-setuptools python-dev python-pip
 sudo apt-get -y install git-core
 sudo apt-get -y install git-flow
 rm "$HOME/.gitconfig"
-ln -s "$HOME/lib/dotfiles/.gitconfig" "$HOME/.gitconfig"
+ln -s "$HOME/.dotfiles/.gitconfig" "$HOME/.gitconfig"
 
 # Install these dotfiles locally
-git clone https://github.com/paulkiernan/dotfiles $HOME/lib/dotfiles
+git clone https://github.com/paulkiernan/dotfiles $HOME/.dotfiles
 
 # Install ZSH stuff
 echo "Installing ZSH"
 chsh -s /bin/zsh
-git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/lib/oh-my-zsh
-git clone git://github.com/sjl/z-zsh $HOME/lib/z
+git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.dotfiles/oh-my-zsh
+git clone git://github.com/sjl/z-zsh $HOME/.dotfiles/z
 rm ~/.zshrc
-ln -s "$HOME/lib/dotfiles/.zshrc" "$HOME/.zshrc"
-ln -s "$HOME/lib/dotfiles/zsh" "$HOME/lib/oh-my-zsh/custom"
-
-# Install git-flow
-git clone git://github.com/bobthecow/git-flow-completion.git $HOME/lib/git-flow-completion
+ln -s "$HOME/.dotfiles/.zshrc" "$HOME/.zshrc"
+ln -s "$HOME/.dotfiles/zsh" "$HOME/.dotfiles/oh-my-zsh/custom"
 
 # Install other config settings
 rm "$HOME/.gitconfig"
-ln -s "$HOME/lib/dotfiles/.gitconfig" "$HOME/.gitconfig"
-ln -s "$HOME/lib/dotfiles/.tmux.conf" "$HOME/.tmux.conf"
-ln -s "$HOME/lib/dotfiles/.tmux.conf" "$HOME/.byoburc.tmux"
+ln -s "$HOME/.dotfiles/.gitconfig" "$HOME/.gitconfig"
+ln -s "$HOME/.dotfiles/.tmux.conf" "$HOME/.tmux.conf"
+ln -s "$HOME/.dotfiles/.tmux.conf" "$HOME/.byoburc.tmux"
 
 # Keep EC2 connections from periodically hanging up
 sudo echo "KeepAlive yes" >> /etc/ssh/sshd_config
@@ -45,8 +42,8 @@ sudo echo "ClientAliveInterval 60" >> /etc/ssh/sshd_config
 
 # Install Steve Francia's awesome vim config
 curl --insecure http://j.mp/spf13-vim3 -L -o - | sh
-ln -s "$HOME/lib/dotfiles/.vimrc.local" "$HOME/.vimrc.local"
-ln -s "$HOME/lib/dotfiles/.vimrc.bundles.local" "$HOME/.vimrc.bundles.local"
+ln -s "$HOME/.dotfiles/.vimrc.local" "$HOME/.vimrc.local"
+ln -s "$HOME/.dotfiles/.vimrc.bundles.local" "$HOME/.vimrc.bundles.local"
 
 # Done!
 echo "All done! Log out of all open sessions to install new env!"
