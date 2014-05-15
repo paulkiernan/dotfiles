240dc4721dfd3ab14b69dd8c813615dd2d20152c
Paul Kiernan <paul.kiernan@moat.com>
2014-05-15 18:30:12 -0400

    Tee correctly

--- bootstrap.sh
+++ bootstrap.sh
@@ -22,10 +22,10 @@ if [ "$UNAME_STR" == 'Linux' ]; then
     KEEPALIVE="KeepAlive yes"
     ALIVETIMEOUT="ClientAliveInterval 60"
     if ! sudo grep -Fxq "$KEEPALIVE" /etc/ssh/sshd_config; then
-        echo "$KEEPALIVE" >> sudo tee -a /etc/ssh/sshd_config
-    fi
+        echo "$KEEPALIVE" | sudo tee -a /etc/ssh/sshd_config
+    filename
     if ! sudo grep -Fxq "$ALIVETIMEOUT" /etc/ssh/sshd_config; then
-        echo "$ALIVETIMEOUT" >> sudo tee -a /etc/ssh/sshd_config
+        echo "$ALIVETIMEOUT" | sudo tee -a /etc/ssh/sshd_config
     fi
 
     echo ""
