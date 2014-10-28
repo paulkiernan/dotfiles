04f7217d317537e539111b92c804578c653cc1ed
Paul Kiernan <paul@chartbeat.com>
2014-10-28 10:53:26 -0400

    Add random deps to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -28,8 +28,8 @@ if [ "$UNAME_STR" == 'Linux' ]; then
 elif [ "$UNAME_STR" == 'Darwin' ]; then
     # Install brew, the package manager for drunks
     hash brew 2>/dev/null || ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
-    brew install python sl vim byobu tree
-    pip install ipython virtualenvwrapper
+    brew install python sl vim byobu tree wget
+    pip install ipython virtualenvwrapper Pygments
 fi
 
 rm -f "$HOME/.gitconfig"
