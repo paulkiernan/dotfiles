9455bd0f32965739fa32f6d5b96877cb0340efd6
Paul Kiernan <paul@chartbeat.com>
2016-05-03 16:36:40 -0400

    Add htop dep

--- bootstrap.sh
+++ bootstrap.sh
@@ -30,7 +30,7 @@ elif [ "$UNAME_STR" == 'Darwin' ]; then
     hash brew 2>/dev/null || ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
     brew install python sl vim byobu tree wget
     brew install michaeldfallen/formula/git-radar
-    brew install archey
+    brew install archey htop
     pip install ipython virtualenvwrapper Pygments
 fi
 
