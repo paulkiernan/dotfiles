992343c68c9efb5e85b68098287c7a7acd66a872
Paul Kiernan <paul.kiernan@reddit.com>
2019-05-24 02:07:10 -0700

    The 4 year culling

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,49 +2,55 @@
 
 set -eux
 
-# Definitions
-DOTFILES_DIR=$HOME/.dotfiles
-OH_MY_ZSH_DIR=$DOTFILES_DIR/oh-my-zsh
-Z_COMPLETION_DIR=$DOTFILES_DIR/z
+KERNEL=$(uname)
+OH_MY_ZSH_DIR="$HOME/.zsh/oh-my-zsh"
+VUNDLE_INSTALL_DIR="$HOME/.vim/bundle/Vundle.vim"
+DEFAULT_SYSTEM_PYTHON="3.7.3"
 
-
-lnif() {
-    # Softlink if not already exists
-    if [ ! -e $2 ] ; then
-        ln -s $1 $2
+if [ "$KERNEL" == 'Linux' ]; then
+    DISTRO=$(lsb_release -sd | tr -d '"' | awk '{print $1;}')
+    if [ "$DISTRO" == 'Arch' ]; then
+        echo ""
+        echo ">> Provisioning for Arch Linux"
+        echo ">> Installing essential linux dev tools using pacman"
+        echo ""
+        sudo pacman -Syu
+        sudo pacman -S \
+            git \
+            pyenv \
+            stow \
+            tree \
+            zsh
+        yay -Sy \
+            githud \
+            pyenv-virtualenv \
+            yay
+    elif [ "$DISTRO" == 'Ubuntu' ]; then
+        echo ""
+        echo ">> Provisioning for Ubuntu"
+        echo ">> Installing essential linux dev tools using apt-get\n"
+        echo ""
+        sudo apt-get update
+        sudo apt-get -y install \
+            sl \
+            curl \
+            build-essential \
+            vim-nox \
+            elinks \
+            tree \
+            git-core \
+            ctags \
+            zsh \
+            tree \
+            htop \
+            git
+        git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+        git clone https://github.com/yyuu/pyenv-virtualenvwrapper.git ~/.pyenv/plugins/pyenv-virtualenvwrapper
+    else
+        echo "Never heard of that Pokemon."
+        exit 1
     fi
-    if [ -L $2 ] ; then
-        ln -sf $1 $2
-    fi
-}
-
-
-UNAME_STR=$(uname)
-if [ "$UNAME_STR" == 'Linux' ]; then
-    echo ""
-    echo ">> Installing essential linux dev tools using apt-get"
-    echo ""
-    sudo apt-get update
-    sudo apt-get -y install \
-        sl \
-        curl \
-        bash-completion \
-        build-essential \
-        vim-nox \
-        byobu \
-        elinks \
-        tree \
-        python-dev \
-        python-pip \
-        git-core \
-        ctags \
-        zsh \
-        tree \
-        htop \
-        git
-    git clone https://github.com/yyuu/pyenv.git ~/.pyenv
-    git clone https://github.com/yyuu/pyenv-virtualenvwrapper.git ~/.pyenv/plugins/pyenv-virtualenvwrapper
-elif [ "$UNAME_STR" == 'Darwin' ]; then
+elif [ "$KERNEL" == 'Darwin' ]; then
     echo ""
     echo ">> Installing brew and other mac shenanigans"
     echo ""
@@ -67,7 +73,6 @@ elif [ "$UNAME_STR" == 'Darwin' ]; then
         wget \
         coreutils \
         reattach-to-user-namespace \
-        jenv \
         scala \
         gnu-getopt \
         bash \
@@ -75,69 +80,38 @@ elif [ "$UNAME_STR" == 'Darwin' ]; then
     brew tap caskroom/versions
     brew cask install caffeine
     brew tap gbataille/homebrew-gba
-    brew install githud
 fi
 
 echo ""
-echo ">> Installing configurable Python system"
+echo ">> Installing Python $DEFAULT_SYSTEM_PYTHON as default"
 echo ""
 eval "$(pyenv init -)"
-pyenv install 3.7.0
-pyenv global 3.7.0
+pyenv install -f $DEFAULT_SYSTEM_PYTHON
+pyenv global $DEFAULT_SYSTEM_PYTHON
 pip install pygments
 
-rm -f "$HOME/.gitconfig"
-lnif -s "$DOTFILES_DIR/.gitconfig" "$HOME/.gitconfig"
-
-echo ""
-echo ">> Creating the local lib directory for dotfiles"
-echo ""
-if [ ! -d $DOTFILES_DIR ]; then
-    # Clone into dotfiles for the first time
-    git clone https://github.com/paulkiernan/dotfiles $DOTFILES_DIR
-elif [ -d $DOTFILES_DIR -a -d $DOTFILES_DIR/.git ]; then
-    cd $DOTFILES_DIR
-    git pull origin master
-    cd $HOME
-fi;
-
-# Install ZSH stuff
-echo "Installing ZSH"
+echo "Installing/Upgrading  ZSH"
+# Install oh-my-zsh or update if already installed
 if [ ! -d $OH_MY_ZSH_DIR ]; then
     git clone git://github.com/robbyrussell/oh-my-zsh.git $OH_MY_ZSH_DIR
 elif [ -d $OH_MY_ZSH_DIR -a -d $OH_MY_ZSH_DIR/.git ]; then
-    cd $OH_MY_ZSH_DIR 
-    git pull origin master
-    cd $HOME
+    git --git-dir=$OH_MY_ZSH_DIR/.git pull origin master
 fi
 
-BONSAI_TREE_INSTALL_LOCATION="$HOME/.bonsai"
-if [ ! -d $BONSAI_TREE_INSTALL_LOCATION ]; then
-    git clone https://gitlab.com/jallbrit/bonsai.sh $BONSAI_TREE_INSTALL_LOCATION
-elif [ -d $BONSAI_TREE_INSTALL_LOCATION -a -d $BONSAI_TREE_INSTALL_LOCATION/.git ]; then
-    cd $BONSAI_TREE_INSTALL_LOCATION
-    git pull origin master
-    cd $HOME
+# Install Vundle (updates are managed by `BundleUpdate`
+if [ ! -d "$VUNDLE_INSTALL_DIR" ]; then
+    git clone https://github.com/VundleVim/Vundle.vim.git $VUNDLE_INSTALL_DIR
 fi
 
-# Set up symlinks!
-mkdir -p "$HOME/.byobu"
-lnif "$DOTFILES_DIR/.zshrc" "$HOME/.zshrc"
-lnif "$DOTFILES_DIR/zsh" "$OH_MY_ZSH_DIR/custom"
-lnif "$DOTFILES_DIR/.gitconfig" "$HOME/.gitconfig"
-lnif "$DOTFILES_DIR/.tmux.conf" "$HOME/.tmux.conf"
-lnif "$DOTFILES_DIR/.tmux.conf" "$HOME/.byobu/.tmux.conf"
-lnif "$DOTFILES_DIR/.vimrc" "$HOME/.vimrc"
-lnif "$DOTFILES_DIR/.vimrc.bundles" "$HOME/.vimrc.bundles"
-lnif "$DOTFILES_DIR/.dockerrc" "$HOME/.dockerrc"
-
-# Download vundle to bootstrap vimconfig
-VUNDLE_INSTALL_LOCATION="$HOME/.vim/bundle/vundle"
-if [ ! -d "$VUNDLE_INSTALL_LOCATION" ]; then
-    mkdir -p $VUNDLE_INSTALL_LOCATION
-    git clone https://github.com/VundleVim/Vundle.vim.git $VUNDLE_INSTALL_LOCATION
-fi
+# Set up all symlinks
+stow -t $HOME alacritty
+stow -t $HOME docker
+stow -t $HOME git
+stow -t $HOME tmux
+stow -t $HOME vim
+stow -t $HOME zsh
+stow -t $HOME scripts
 
 # Done!
-sudo chsh -s /bin/zsh
-acho "All done! Log out of all open sessions to install new env!"
+usermod -s /bin/zsh "$USER"
+echo "All done! Log out of all open sessions to install new env!"
